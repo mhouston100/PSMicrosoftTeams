fb89559f3dd7b934ce7a67ccc33a90c9ba5ae1e3
admin Matthew Houston <adminMHouston@camden.nsw.gov.au>
2018-12-20 15:25:04 +1100

    Removed 'Images' and replaced with BASE64 Encoded images inline

--- Send-TestMessage.ps1
+++ Send-TestMessage.ps1
@@ -2,9 +2,12 @@
 param (
     [parameter(Mandatory = $true, HelpMessage = "Paste the URI for your webhook")]
 	[ValidateNotNullOrEmpty()]
-	[string]$URI
+    [string]$URI,
+    [parameter(HelpMessage = "Test image")]
+	[ValidateNotNullOrEmpty()]
+    [string]$messageType = "Success"
 )
 
 Import-Module .\PSMicrosoftTeams.psm1
 
-Send-TeamChannelMessage -messageType Success -messageTitle "This is a test message Title" -messageBody "This is a test message Body" -activityTitle "This is a test message Activity Title" -activitySubtitle "This is a test message Activity Subtitle" -URI $URI
+Send-TeamChannelMessage -messageType $messageType -messageTitle "This is a test message Title" -messageBody "This is a test message Body" -activityTitle "This is a test message Activity Title" -activitySubtitle "This is a test message Activity Subtitle" -URI $URI
